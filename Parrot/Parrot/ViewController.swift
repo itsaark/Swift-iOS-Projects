7dbbcceb9042de412bcb1b096a40bb3c395696e5
Arjun <itsarjunn@gmail.com>
2016-11-17 22:25:00 +0530

    Adding Parrot Project

--- Parrot/Parrot/ViewController.swift
+++ Parrot/Parrot/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import VisualRecognitionV3
+import AlamofireImage
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //IBM Watson Key
+    let apiKey = "YOUR KEY"
+    let version = "2016-11-17"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +28,73 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.imageTitle.text = "Tap the camera button"
+    }
+
+    
+    @IBAction func getImage(_ sender: Any) {
+        
+        let button = sender as! UIBarButtonItem
+        button.isEnabled = false
+        
+        let randomNumber = Int(arc4random_uniform(1500))
+        let urlString = "https://unsplash.it/400/700?image=\(randomNumber)"
+
+        Alamofire.request(urlString).responseImage { response in
+            
+            if let image = response.result.value {
+                
+                DispatchQueue.main.async {
+                    
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        let imageUrlIMB = URL(string: "https://unsplash.it/50/100?image=\(randomNumber)")!
+        
+        let failure = {(error:Error) in
+            
+            DispatchQueue.main.async {
+                self.imageTitle.text = "Image could not be processed"
+                button.isEnabled = true
+            }
+            
+            print(error)
+        }
+        let visualRecognition = VisualRecognition(apiKey: apiKey, version: version)
+        visualRecognition.classify(image: imageUrlIMB.absoluteString , failure: failure) { classifiedImages in
+            
+            if let classifiedImage = classifiedImages.images.first {
+                
+                if let classification = classifiedImage.classifiers.first?.classes.first?.classification{
+                    
+                    DispatchQueue.main.async {
+                        
+                        self.imageTitle.text = classification
+                        button.isEnabled = true
+                    }
+                }else{
+                    
+                    DispatchQueue.main.async {
+                        self.imageTitle.text = "Image cannot be classified"
+                        button.isEnabled = true
+                    }
+                }
+            }else{
+                
+                DispatchQueue.main.async {
+                    
+                    self.imageTitle.text = "Image cannot be classified"
+                    button.isEnabled = true
+                }
+            }
+        }
+        
+        
     }
 
 
---
+++ Parrot/README.md
@@ -0,0 +1,20 @@
+# Parrot-Image Recognition App
+
+![Platform iOS](https://img.shields.io/badge/Platform-iOS-blue.svg) ![Programming Language Swift](https://img.shields.io/badge/Programming_Language-Swift-orange.svg)
+
+
+Parrot is an image recognition app. It uses IBM Watson Image Recognition API to classify images. I'll soon add a live camera feature which can spontaneously recognize the objects
+
+I have used the following libraries while creating this application:
+
+  - Alamofire: to make HTTP requests and to download images
+  - VisualRecognitionV3: To classify images
+
+I used [Unsplash](https://unsplash.it/) to generate images randomly
+
+
+
+
+> Here is the demo of the app
+
+![](http://i.giphy.com/3oz8xXn8zDyPcpgzWo.gif)
